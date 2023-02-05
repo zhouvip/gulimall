d72f882856b8d573b85a8767cee8fceeff935a00
zhouvip <1136025373@qq.com>
2023-02-05 16:31:31 +0800

    1.springboot版本降为2.6.6 2.mapper.xml笔误gulimail改成gulimall

--- gulimall-product/src/test/java/com/nanjing/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/nanjing/gulimall/product/GulimallProductApplicationTests.java
@@ -2,10 +2,15 @@ package com.nanjing.gulimall.product;
 
 import com.nanjing.gulimall.product.entity.BrandEntity;
 import com.nanjing.gulimall.product.service.BrandService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@Slf4j
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class GulimallProductApplicationTests {
 
