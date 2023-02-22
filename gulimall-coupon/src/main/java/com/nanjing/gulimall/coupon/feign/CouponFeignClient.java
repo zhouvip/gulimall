284d8e57fd954af77256efb6e24b5f441da3ceae
ZHOU <1136025373@qq.com>
2023-02-22 14:37:18 +0800

    feign调用注释

--- gulimall-coupon/src/main/java/com/nanjing/gulimall/coupon/feign/CouponFeignClient.java
+++ gulimall-coupon/src/main/java/com/nanjing/gulimall/coupon/feign/CouponFeignClient.java
@@ -3,7 +3,13 @@ package com.nanjing.gulimall.coupon.feign;
 import com.nanjing.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+/*
+* 关于feign接口，是写在服务提供者一端还是，服务消费者一端？
+* feign接口可以写在服务提供者一端，也可以写在服务消费者一端，
+* 因为启动类的指定了feign接口所在的位置，但是建议写在服务提供者一段，
+* 这样feign接口就可以复用，避免重复编写代码。
+* 可以写在独立的服务提供者的api服务提供者模块，同样可以代码复用。
+*/
 @FeignClient(value = "gulimall-coupon", path = "/coupon/coupon", fallback = CouponFeignClientFallback.class)
 public interface CouponFeignClient {
 
