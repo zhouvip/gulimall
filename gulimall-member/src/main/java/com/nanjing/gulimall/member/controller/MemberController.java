420af4cf0e09b44a1c716dcada7c8076fdb9a013
ZHOU <1136025373@qq.com>
2023-03-10 17:55:00 +0800

    测试feign调用

--- gulimall-member/src/main/java/com/nanjing/gulimall/member/controller/MemberController.java
+++ gulimall-member/src/main/java/com/nanjing/gulimall/member/controller/MemberController.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.Map;
 
 //import org.apache.shiro.authz.annotation.RequiresPermissions;
-import com.nanjing.gulimall.coupon.feign.CouponFeignClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
